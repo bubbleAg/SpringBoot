e74f955f106487b09accfcdd8c2176753a36a4ba
Agnieszka Korowacka <ext.agnieszka.korowacka@rail.bombardier.com>
2020-03-23 17:42:39 +0100

    Response message while exception occurs

--- src/main/java/com/koro/carshomework3/exception/ErrorHandler.java
+++ src/main/java/com/koro/carshomework3/exception/ErrorHandler.java
@@ -1,7 +1,10 @@
 package com.koro.carshomework3.exception;
 
+import com.koro.carshomework3.controller.ResponseMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,13 +13,15 @@ public class ErrorHandler {
 
     @ExceptionHandler(CarNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String carNotFoundExceptionHandler(CarNotFoundException ex) {
-        return ex.getMessage();
+    public ResponseMessage carNotFoundExceptionHandler(CarNotFoundException ex) {
+        ResponseMessage responseMsg = new ResponseMessage(ex.getMessage());
+        return responseMsg;
     }
 
     @ExceptionHandler(CarAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String carAlreadyExistsHandler(CarAlreadyExistsException ex) {
-        return ex.getMessage();
+    public ResponseMessage carAlreadyExistsHandler(CarAlreadyExistsException ex) {
+        ResponseMessage responseMsg = new ResponseMessage(ex.getMessage());
+        return responseMsg;
     }
 }
